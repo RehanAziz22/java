7d4ce407c2aad872df98ed94b22ed4d01486825c
Rehan Aziz <67968249+RehanAziz22@users.noreply.github.com>
2021-03-03 14:03:15 +0500

    Add files via upload
     Write a program to print following output using for loop.
    1
    22
    333
    4444
    55555
    Hint: Use nested for loop.

---
+++ MuhammadRehanBSE20F074.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+
+public class MuhammadRehanBSE20F074 {
+
+    public static void main(String[] args) {
+        {
+        int i, j;
+        for (i = 1; i <= 5; i++)
+        {
+        for (j = 1; j <= i; j++) //Nested for loop
+        {
+        System.out.print(i);
+        }
+        System.out.print("\n");
+        } 
+        }
+}    
+}
