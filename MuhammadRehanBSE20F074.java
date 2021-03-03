81f1fb2e24002991bcd06f005e1f0b94d4ff0735
Rehan Aziz <67968249+RehanAziz22@users.noreply.github.com>
2021-03-03 13:37:47 +0500

    Add files via upload
    Write a program to display table of a number, get input by user.
    Hint: Use for loop.

---
+++ MuhammadRehanBSE20F074.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+
+public class MuhammadRehanBSE20F074 {
+
+    public static void main(String[] args) {
+        {
+        System.out.println("Enter number:");
+            
+        Scanner sc = new Scanner (System.in);
+        int j = sc.nextInt();
+        /* for loop execution */
+        for (int a = 1 ; a <= 12; a++)
+        {
+        System.out.println(j+ " * "+a+ " = " +j*a);
+        }
+        }
+    }
+}    
+
