1c3e223e056c0dc52bc86a7109e800b6c7bc2299
Rehan Aziz <67968249+RehanAziz22@users.noreply.github.com>
2021-03-03 14:36:52 +0500

    Create first 20 int in revers.java
    Java program to print first 20 integers in reverse order.

---
+++ first 20 int in revers.java
@@ -0,0 +1,13 @@
+
+public class MuhammadRehanBSE20F074 {
+    //Java program to print first 20 integers in reverse order.
+    public static void main(String[] args) {
+    
+    int i = 20, n = 1;
+
+    while(i >= n) {
+      System.out.println(i);
+      i--;
+    }
+  }  
+}
