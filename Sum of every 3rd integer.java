0b1b99be5b6e5e44f0f0c67a71e34417c8e810c2
Rehan Aziz <67968249+RehanAziz22@users.noreply.github.com>
2021-03-04 13:04:54 +0500

    Create Sum of every 3rd integer.java
    Write a program using do-while loop that will calculate the sum of every third integer beginning with i=2, (i.e. sum = 2+5+8+11+….) for values of ‘i’ that are less than 100.

---
+++ Sum of every 3rd integer.java
@@ -0,0 +1,15 @@
+public class HelloWorld{
+
+    public static void main(String []args){
+         
+   
+    int i=2, sum=0;
+    
+    do{
+        sum=sum+i;
+        i+=3;  
+    }
+    while(i<=100);
+        System.out.println("Sum is :"+sum);
+     }
+}
